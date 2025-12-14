bf057ee4337f74e4f49714ac9fad067bbc18ace4
lorenayan <lorena.yantianyi1020@gmail.com>
2025-12-14 11:00:35 -0500

    Update dependency

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ pip install -r my_requirements.txt
 # Change CUDA, torch, abiTRUE/FALSE, python version in the following line as needed
 wget -nv https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp310-cp310-linux_x86_64.whl && pip install --no-cache-dir flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
 pip install flashinfer-python==0.5.3
+pip install lm_eval==0.4.9.2
+pip install lm-eval[math]
 
-bash scripts/build_omegaconf_hydra_w_antlr_4111.sh
+bash scripts/build_omegaconf_hydra_w_antlr_4110.sh
 
 python -c "import nltk; nltk.download('punkt_tab')"
--- scripts/build_omegaconf_hydra_w_antlr_4110.sh
+++ scripts/build_omegaconf_hydra_w_antlr_4110.sh
@@ -1,5 +1,5 @@
 
-# TODO: temporary fixes for omegaconf/hydra to use antlr 4.11.1 (needed by parse_latex for training on hendrycks math dataset)
+# TODO: temporary fixes for omegaconf/hydra to use antlr 4.11.0 (needed by parse_latex for training on hendrycks math dataset)
 
 # Check if Java is installed, if not install OpenJDK 17
 if ! command -v java &> /dev/null
@@ -26,26 +26,26 @@ git clone https://github.com/facebookresearch/hydra
   git checkout v1.3.2
 )
 
-# download the antlr 4.11.1 binary:
-wget https://www.antlr.org/download/antlr-4.11.1-complete.jar
+# download the antlr 4.11.0 binary:
+wget https://www.antlr.org/download/antlr-4.11.0-complete.jar
 
 # replace the old antlr binaries in BOTH omegaconf and hydra
 rm omegaconf/build_helpers/bin/antlr-4.9.3-complete.jar
 rm hydra/build_helpers/bin/antlr-4.9.3-complete.jar
-cp antlr-4.11.1-complete.jar omegaconf/build_helpers/bin/
-cp antlr-4.11.1-complete.jar hydra/build_helpers/bin/
+cp antlr-4.11.0-complete.jar omegaconf/build_helpers/bin/
+cp antlr-4.11.0-complete.jar hydra/build_helpers/bin/
 
-# update all references to 4.9.3 / 4.9.* to 4.11.1 in omegaconf + hydra only
+# update all references to 4.9.3 / 4.9.* to 4.11.0 in omegaconf + hydra only
 (
   cd omegaconf
-  grep -ErlI '4\.9\.[\*3]' . | xargs sed -E -i 's/4\.9\.[\*3]/4.11.1/g' || true
+  grep -ErlI '4\.9\.[\*3]' . | xargs sed -E -i 's/4\.9\.[\*3]/4.11.0/g' || true
 )
 (
   cd hydra
-  grep -ErlI '4\.9\.[\*3]' . | xargs sed -E -i 's/4\.9\.[\*3]/4.11.1/g' || true
+  grep -ErlI '4\.9\.[\*3]' . | xargs sed -E -i 's/4\.9\.[\*3]/4.11.0/g' || true
 )
 
-pip install "antlr4-python3-runtime==4.11.1"
+pip install "antlr4-python3-runtime==4.11.0"
 
 pip install build
 
@@ -61,9 +61,9 @@ pip install build
 pip install omegaconf/dist/omegaconf-2.3.0-*.whl
 pip install hydra/dist/hydra_core-1.3.2-*.whl
 
-# verify that the Python runtime installed is 4.11.1
+# verify that the Python runtime installed is 4.11.0
 pip list 2>/dev/null | grep antlr4
 
 # cleanup
-rm -rf omegaconf hydra antlr-4.11.1-complete.jar
-#bash scripts/build_omegaconf_hydra_w_antlr_4111.sh
+rm -rf omegaconf hydra antlr-4.11.0-complete.jar
+#bash scripts/build_omegaconf_hydra_w_antlr_4110.sh
